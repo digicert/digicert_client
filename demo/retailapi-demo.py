870d5969fa4e5d133a76f3226bfe39812933dd0b
Jeff <jeff.fischer@digicert.com>
2014-10-03 14:32:49 -0600

    stubs

---
+++ demo/retailapi-demo.py
@@ -0,0 +1,7 @@
+__author__ = 'jfischer'
+
+def call_home():
+    print 'Python FTW!'
+
+if __name__ == '__main__':
+    call_home()
---
+++ plugin/digicert.py
@@ -0,0 +1,11 @@
+# copyright license from digicert??
+
+__author__ = 'fishy, mryan'
+
+# inherit the certificatepluginbase class
+
+# implement the methods in the parent
+
+# check for authentication, any validation of input parameters
+
+# call our digicert client
