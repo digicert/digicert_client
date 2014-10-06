3fbdb560a56519c6caa66e5bd063d06abb63246e
Matt Ryan <matt.ryan@digicert.com>
2014-10-06 11:47:14 -0600

    Complete order certificate command

--- digicert/api/commands/__init__.py
+++ digicert/api/commands/__init__.py
@@ -1,21 +1,83 @@
 #!/usr/bin/env python
 
-import json
+from base64 import b64encode
+from urllib import urlencode
+from httplib import HTTPSConnection
+
 
 class RetailCommand:
     customer_name = None
     customer_api_key = None
     response_type = 'json'
 
+    _digicert_api_host = 'www.digicert.com'
+    _digicert_api_path = '/clients/retail/api/'
+
+    _headers = {'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'}
+
     def __init__(self, customer_name, customer_api_key, **kwargs):
         self.customer_name = customer_name
         self.customer_api_key = customer_api_key
         for key, value in kwargs.items():
-            if not self.process_special(key, value):
+            if not self._process_special(key, value):
                 setattr(self, key, value)
+        self.set_header('Authorization', self.get_authorization())
 
-    def process_special(self, key, value):
+    def _process_special(self, key, value):
         pass
+
+    def get_authorization(self):
+        """
+        Retrieve the encoded authorization header value that is expected
+        by the Retail API.
+        :return: Authorization value
+        """
+        return b64encode(':'.join([self.customer_name, self.customer_api_key]))
+
+    def get_params(self):
+        """
+        Retrieve the urlencoded set of parameters that will be sent
+        as the payload of the command.
+        :return: Urlencoded payload
+        """
+        return urlencode(self.__dict__)
+
+    def set_header(self, header_name, header_value):
+        """
+        Update the headers to be sent with the pair provided.
+        This will add this header to the list of headers if
+        this header has not already been set, or overwrite the
+        value already set for this header if one exists.
+        Take care my child.
+        :param header_name: Name of the header to set
+        :param header_value: Value to set for this header
+        :return:
+        """
+        self._headers[header_name] = header_value
+
+    def get_headers(self):
+        """
+        Retrieve the dictionary of headers to be sent with the request.
+        :return: Header dictionary
+        """
+        return self._headers
+
+    def send(self, conn=None):
+        """
+        Send this command to the DigiCert Retail API.
+        :param conn: Connection instance to use for sending the request. If no
+        instance is provided, an HTTPSConnection will be used with the default
+        DigiCert API hostname.
+        :return: Response from connection request.
+        """
+        if conn is None:
+            conn = HTTPSConnection(self._digicert_api_host)
+        conn.request('POST', self._digicert_api_path, self.get_params(), self.get_headers())
+        response = conn.getresponse()
+        conn.close()
+        return response
+
 
 class OrderCertificateCommand(RetailCommand):
     certificate_type = None
@@ -24,7 +86,7 @@ class OrderCertificateCommand(RetailCommand):
     server_type = -1
     org_unit = None
     common_name = None
-    sans = []
+    sans = None
     org_name = None
     org_addr1 = None
     org_addr2 = None
@@ -74,17 +136,11 @@ class OrderCertificateCommand(RetailCommand):
         self.org_contact_email = org_contact_email
         self.org_contact_telephone = org_contact_telephone
 
-    def process_special(self, key, value):
+    def _process_special(self, key, value):
         if 'server_type' == key:
             self.server_type = int(value)
             return True
-        elif 'sans' == key:
-            for san in value.split(','):
-                if len(san):
-                    self.sans.append(san.strip())
-            return True
         return False
-
 
 
 if __name__ == '__main__':
--- tests/TestOrderCertificateCommand.py
+++ tests/TestOrderCertificateCommand.py
@@ -2,12 +2,16 @@
 
 import unittest
 import json
+from urllib import urlencode
+from urlparse import parse_qs
 from digicert.api.commands import OrderCertificateCommand
 
 
 class TestOrderCertificateCommand(unittest.TestCase):
     _customer_name = '12345'
     _customer_api_key = 'abapsdrtaewrh89249sbs89as0d'
+    _api_host = 'www.digicert.com'
+    _api_path = '/clients/retail/api/'
     _requireds_dict = {
         'certificate_type': 'sslplus',
         'csr': 'fakecsr',
@@ -54,23 +58,89 @@ class TestOrderCertificateCommand(unittest.TestCase):
     def validate_opt(self, o):
         self.assertEqual(o.server_type, 2)
         self.assertEqual(o.org_unit, 'FakeCo')
-        self.assertEqual(o.sans, ['Fake Company', 'Fake Inc.'])
+        self.assertEqual(o.sans, 'Fake Company, Fake Inc.')
         self.assertEqual(o.org_addr2, 'Infinitieth Floor')
         self.assertEqual(o.telephone, '2345556789')
         self.assertEqual(o.org_contact_job_title, 'CTO')
         self.assertEqual(o.org_contact_telephone_ext, '5150')
 
+    def getOcc(self, additional_args):
+        return OrderCertificateCommand(customer_name=self._customer_name,
+                                       customer_api_key=self._customer_api_key,
+                                       **additional_args)
+
     def test_construct(self):
-        self.validate(OrderCertificateCommand(customer_name=self._customer_name,
-                                              customer_api_key=self._customer_api_key,
-                                              **self._requireds_dict))
+        self.validate(self.getOcc(self._requireds_dict))
 
     def test_construct_optionals(self):
-       occ = OrderCertificateCommand(customer_name=self._customer_name,
-                                     customer_api_key=self._customer_api_key,
-                                     **self._optionals_dict)
-       self.validate(occ)
-       self.validate_opt(occ)
+        occ = self.getOcc(self._optionals_dict)
+        self.validate(occ)
+        self.validate_opt(occ)
+
+    def test_get_auth(self):
+        occ = self.getOcc(self._requireds_dict)
+        self.assertEqual('MTIzNDU6YWJhcHNkcnRhZXdyaDg5MjQ5c2JzODlhczBk', occ.get_authorization())
+
+    def test_get_payload(self):
+        actuals = dict(
+            {'customer_api_key': self._customer_api_key, 'customer_name': self._customer_name}.items() +
+            self._requireds_dict.items())
+        occ = self.getOcc(self._requireds_dict)
+        payload = occ.get_params()
+        self.assertTrue(len(payload) > 0)
+        d = parse_qs(payload)
+        for k in d.keys():
+            self.assertEqual([actuals[k]], d[k])
+        self.assertEqual(len(actuals), len(d))
+
+    def test_get_payload_optionals(self):
+        actuals = dict(
+            {'customer_api_key': self._customer_api_key, 'customer_name': self._customer_name}.items() +
+            self._optionals_dict.items())
+        occ = self.getOcc(self._optionals_dict)
+        payload = occ.get_params()
+        self.assertTrue(len(payload) > 0)
+        d = parse_qs(payload)
+        for k in d.keys():
+            self.assertEqual([actuals[k]], d[k])
+        self.assertEqual(len(actuals), len(d))
+
+    def test_get_headers(self):
+        occ = self.getOcc(self._requireds_dict)
+        self.assertEqual('application/x-www-form-urlencoded', occ.get_headers()['Content-Type'])
+        self.assertEqual('application/json', occ.get_headers()['Accept'])
+
+    def test_send(self):
+        class MockConnection:
+            host = None
+            method = None
+            path = None
+            params = None
+            headers = None
+
+            def __init__(self, host):
+                self.host = host
+
+            def request(self, method, path, params, headers):
+                self.method = method
+                self.path = path
+                self.params = params
+                self.headers = headers
+
+            def getresponse(self):
+                pass
+
+            def close(self):
+                pass
+
+        occ = self.getOcc(self._requireds_dict)
+        mc = MockConnection(self._api_host)
+        occ.send(conn=mc)
+        self.assertEqual(self._api_host, mc.host)
+        self.assertEqual(self._api_path, mc.path)
+        self.assertEqual('POST', mc.method)
+        self.assertEqual(occ.get_params(), mc.params)
+        self.assertEqual(occ.get_headers(), mc.headers)
 
 
 if __name__ == '__main__':
