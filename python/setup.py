843d83664877c9e4db43b94df83b2cf5540c9959
Nikolay Denev <nikolay@thousandeyes.com>
2019-11-28 09:00:59 +0000

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -5,6 +5,7 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+
 setup(
     name='digicert_client',
     version='1.1.03',
@@ -14,7 +15,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Security',
     ],
     url='https://github.com/digicert/digicert_client/python',
@@ -22,9 +23,11 @@ setup(
     author_email='support@digicert.com',
     license='MIT',
     zip_safe=False,
-    packages=find_packages(exclude=['tests.*', '*.tests.*', '*.tests', 'tests']),
+    packages=find_packages(exclude=['tests.*',
+                                    '*.tests.*',
+                                    '*.tests',
+                                    'tests']),
     include_package_data=True,
     test_suite='nose.collector',
     tests_require=['nose'],
 )
-
